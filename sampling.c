cb92e1cf2cd4fda02e6d77355933d979025e7705
Guilherme Grendel <guilherme.grendel@unicamperacing.com.br>
2024-12-09 22:31:31 -0300

    Fixed lowVarianceSampler

--- sampling.c
+++ sampling.c
@@ -4,21 +4,25 @@
 #include "headers/constants.h"
 
 int* lowVarianceSampler(Particle *particles, float weights[], int numParticles){
-    int c, i, *indexes;
-    float u, r;
+    int i, *indexes;
+    float totalWeight, c, u, r;
 
     indexes = malloc(numParticles * sizeof(int));
 
+    totalWeight = 0.0;
+
+    for(int n = 0; n < numParticles; n++) totalWeight += weights[n];
+
     i = 0;
     u = 0;
-    c = weights[0];
+    c = weights[0] / totalWeight;
     r = ((float) rand()) / RAND_MAX;
 
     for(int j = 0; j < numParticles; j++){
         u = (r + j) / numParticles;
         while(u > c){
             i++;
-            c += weights[i];
+            c += weights[i] / totalWeight;
         }
         indexes[j] = i;
     }
